5d6b782d30ee527606b61745b5a2006d52d27e7c
celestehb-star <celestehb@berkeley.edu>
2025-12-14 16:57:14 -0800

    Refactor map initialization and GeoJSON loading

--- script.js
+++ script.js
@@ -3,22 +3,19 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2VsZXN0ZWhiIiwiYSI6ImNtaDlyN2F2dDFmZXQybG9ha
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/celestehb/cmj6eb5or001u01r7fmpxb829',
-  center: [-0.1278, 51.5074], // London
+  center: [-0.1278, 51.5074],
   zoom: 10
 });
 
 map.on('load', () => {
-  // Load your existing GeoJSON Tube lines
-  fetch('tube_lines.geojson') // make sure this matches your file name
+  fetch('tube_lines.geojson')
     .then(response => response.json())
     .then(data => {
-      // Add the GeoJSON as a source
       map.addSource('tubeLines', {
         type: 'geojson',
         data: data
       });
 
-      // Draw the lines on the map
       map.addLayer({
         id: 'tubeLinesLayer',
         type: 'line',
@@ -28,11 +25,10 @@ map.on('load', () => {
           'line-cap': 'round'
         },
         paint: {
-          'line-color': ['get', 'color'], // uses color from GeoJSON property
+          'line-color': ['get', 'color'],
           'line-width': 5
         }
       });
     })
     .catch(err => console.error('Error loading GeoJSON:', err));
 });
-
