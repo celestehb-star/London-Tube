184c2cb196c221e609906f4d3be217fe3d44d04f
celestehb-star <celestehb@berkeley.edu>
2025-12-14 16:33:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ const map = new mapboxgl.Map({
   center: [-0.1278, 51.5074], // London
   zoom: 10
 });
+
+// Wait until map loads
+map.on('load', () => {
+  // Fetch your existing GeoJSON file
+  fetch('tube_lines.geojson') // make sure the filename matches exactly
+    .then(response => response.json())
+    .then(data => {
+      // Add the GeoJSON as a source
+      map.addSource('tubeLines', { type: 'geojson', data: data });
+
+      // Add a line layer to draw your Tube lines
+      map.addLayer({
+        id: 'tubeLinesLayer',
+        type: 'line',
+        source: 'tubeLines',
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': ['get', 'color'], // assumes each feature has a "color" property
+          'line-width': 5
+        }
+      });
+    })
+    .catch(err => console.error('Error loading GeoJSON:', err));
+});
